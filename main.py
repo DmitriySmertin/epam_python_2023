0eb6ec591c1a9363270a2d64ce411e8317136024
Dmitrii Smertin <dmitrii.smertin@ukg.com>
2023-03-27 13:00:33 +0300

    finish

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from datetime import datetime
+from src.hw5.hw5_1 import KeyValueStorage
 
-dt_string = "12AA-11-20"
-new_string = []
-for symbol in dt_string:
-    if not symbol.isalpha():
-        new_string.append(symbol)
+storage = KeyValueStorage("task1.txt")
 
-new_string = ''.join(new_string).strip()
-
-print(new_string)
+print(storage["name"])
+print(storage.name)
+print(storage.power)
--- src/hw5/hw5_1.py
+++ src/hw5/hw5_1.py
@@ -22,3 +22,26 @@ In case of attribute clash existing built-in attributes take precedence.
 In case when value cannot be assigned to an attribute (for example when there's a line `1=something`) ValueError should be raised.
 File size is expected to be small, you are permitted to read it entirely into memory.
 """
+
+
+class KeyValueStorage:
+    def __init__(self, file_path):
+        with open(file_path, 'r') as file:
+            self.data = {}
+            for line in file:
+                key, value = line.strip().split('=')
+                if key.isnumeric():
+                    raise ValueError(f"Invalid key name '{key}'")
+                if value.isnumeric():
+                    value = int(value)
+                self.data[key] = value
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return self.__dict__[attr]
+        if attr not in self.data:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{attr}'")
+        return self.data[attr]
+
+    def __getitem__(self, key):
+        return self.__getattr__(key)
