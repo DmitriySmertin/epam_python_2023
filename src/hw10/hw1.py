9215ffa090fc01cd3e480ba344247410a0e1e499
Dmitrii Smertin <dmitrii.smertin@ukg.com>
2023-04-18 13:10:13 +0300

    hw10/1 complete

--- src/hw10/hw1.py
+++ src/hw10/hw1.py
@@ -33,3 +33,24 @@ class SizesEnum(metaclass=SimplifiedEnum):
 assert ColorsEnum.RED == "RED"
 assert SizesEnum.XL == "XL"
 """
+
+class SimplifiedEnum(type):
+
+    def __new__(mcs, name, bases, dct):
+        cls_instance = super().__new__(mcs, name, bases, dct)
+        for key in cls_instance._keys:
+            setattr(cls_instance, key, key)
+        return cls_instance
+
+    def __prepare__(cls, bases, **kwargs):
+        keys = kwargs.get("_keys", ())
+        return {k: k for k in keys}
+
+class ColorsEnum(metaclass=SimplifiedEnum):
+    _keys = ("RED", "BLUE", "ORANGE", "BLACK")
+
+
+class SizesEnum(metaclass=SimplifiedEnum):
+    _keys = ("XL", "L", "M", "S", "XS")
+
+assert ColorsEnum.RED == "RED"
