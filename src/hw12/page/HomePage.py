8c2993cfbe44c09c5aa9310f95868ff09bbe184c
Dmitrii Smertin <dmitrii.smertin@ukg.com>
2023-05-02 16:46:18 +0300

    added structure

---
+++ src/hw12/page/HomePage.py
@@ -0,0 +1,6 @@
+from src.hw12.page.BasePage import BasePage
+
+
+class HomePage(BasePage):
+    def __init__(self):
+        super().__init__()
---
+++ test/hw12/login_test.py
@@ -0,0 +1,5 @@
+# step 1: click login button
+#   expected result: login and password fields are presented
+# step 2 set up login and password and click login button
+#   expected result: Log out button is presented;  Welcome {username} message is presented
+
